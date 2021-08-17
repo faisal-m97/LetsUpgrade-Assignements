863727166b89a94a947801e183ca20a85b56a392
faisal-m97 <87426982+faisal-m97@users.noreply.github.com>
2021-08-17 18:22:08 +0530

    Delete Assignment1 directory

--- Assignment1/src/Comp1.js
+++
@@ -1,8 +0,0 @@
-function Comp1(){
-
-    return(
-        <h1>Hi!!!  I am Component 1</h1>
-    )
-}
-
-export default Comp1;
--- Assignment1/src/Comp2.js
+++
@@ -1,9 +0,0 @@
-function Comp2(){
-
-    
-    return (
-        <h1>Hello!!! I am Compoment 2</h1>
-    )
-}
-
-export default Comp2;
